622db4bf30833622e49d40892fceda12dfba5f07
Andrés Aragón <andresaragonguerrero@gmail.com>
2026-01-27 12:32:49 +0000

    Se actualizó la estructura del app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import { Hero } from './components/hero/hero';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    Header,
     Footer,
+    Hero,
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
