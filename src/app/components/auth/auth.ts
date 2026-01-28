2b3c000f5e834ccecc4f837c3ea1d7096979fa45
andresaragonguerrero <andresaragonguerrero@gmail.com>
2026-01-28 19:47:39 +0100

    Reestructuración del App y corrección del Auth

--- src/app/components/auth/auth.ts
+++ src/app/components/auth/auth.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   imports: [
-    RouterModule,
+    RouterLink
   ],
   templateUrl: './auth.html',
   styleUrl: './auth.scss',
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 // secciones del home
-import { Hero } from "../../sections/hero/hero";
+import { Hero } from '../../sections/hero/hero';
 import { TicketPurchase } from "../../sections/ticket-purchase/ticket-purchase";
 import { ParkInfo } from "../../sections/park-info/park-info";
 import { Courses } from "../../sections/courses/courses";
