34acdc76453ec1ac804250f7ea57459b43221c56
Andrés Aragón <andresaragonguerrero@gmail.com>
2026-01-30 09:11:04 +0000

    Se actualizó la estructura de la compra de entradas

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,19 +3,15 @@ import { CommonModule } from '@angular/common';
 
 // secciones del home
 import { Hero } from '../../sections/hero/hero';
-import { TicketPurchase } from "../../sections/ticket-purchase/ticket-purchase";
 import { ParkInfo } from "../../sections/park-info/park-info";
 import { Courses } from "../../sections/courses/courses";
 import { Activities } from "../../sections/activities/activities";
-import { TicketForm } from '../../sections/ticket-form/ticket-form';
 
 @Component({
   selector: 'app-home',
   imports: [
     CommonModule,
     Hero,
-    TicketPurchase,
-    TicketForm,
     ParkInfo,
     Courses,
     Activities,
@@ -24,6 +20,5 @@ import { TicketForm } from '../../sections/ticket-form/ticket-form';
   styleUrl: './home.scss',
 })
 export class HomeComponent {
-  // lógica para alternar entre las secciones información de las entradas y su compra
-  showTicketForm = false;
+
 }
--- src/app/pages/tickets/tickets.ts
+++ src/app/pages/tickets/tickets.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 
+import { TicketPurchase } from "../../sections/ticket-purchase/ticket-purchase";
+import { TicketForm } from '../../sections/ticket-form/ticket-form';
+
 @Component({
   selector: 'app-tickets',
-  imports: [],
+  imports: [
+    TicketPurchase,
+    TicketForm,
+  ],
   templateUrl: './tickets.html',
   styleUrl: './tickets.scss',
 })
 export class TicketsComponent {
-
+  // lógica para alternar entre las secciones información de las entradas y su compra
+  showTicketForm = false;
 }
