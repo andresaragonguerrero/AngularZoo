8c8a30216cc0bcec3c4ddfa6ddde44900ce46c7d
Andrés Aragón <andresaragonguerrero@gmail.com>
2026-01-30 19:22:53 +0000

    Se añadió el Routerlink

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { LoginForm } from '../../components/login-form/login-form';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   imports: [
+    RouterLink,
     LoginForm,
   ],
   templateUrl: './login-page.html',
